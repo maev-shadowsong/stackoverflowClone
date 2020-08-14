72945c6e84e4cc56ac9f569c7f3ce7455ddd813e
Rizky WP <rizkywhy@Yahoo.com>
2020-08-14 08:06:12 +0700

    migration

---
+++ database/migrations/2020_08_14_005341_create_tag_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('value');
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('diskusi_id');
+            $table->foreign('diskusi_id')->references('id')->on('diskusi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag');
+    }
+}
